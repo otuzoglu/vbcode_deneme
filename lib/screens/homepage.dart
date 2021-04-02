2855f4d1116ebaaab0e6698ea19aa35bc983c290
a30 <alperotuzoglu@gmail.com>
2021-04-02 07:57:27 +0300

    deneme

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,15 @@
+class denemr extends StatefulWidget {
+  denemr({Key key}) : super(key: key);
+
+  @override
+  _denemrState createState() => _denemrState();
+}
+
+class _denemrState extends State<denemr> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+    );
+  }
+}
